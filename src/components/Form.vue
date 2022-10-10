db8edee4c6634c59f82d3f4a1f5787d4598422f7
Createawish <bezlyudovalera1996@gmail.com>
2022-10-10 16:23:31 +0300

    Feature: add layout Form

---
+++ src/components/Form.vue
@@ -0,0 +1,23 @@
+<template>
+<ElCard>
+    <ElForm>
+
+    </ElForm>
+</ElCard>
+</template>
+
+
+<script>
+
+export default {
+    name: 'Form',
+    component: {},
+    data: () => ({}),
+    computed:{},
+    methods:{}
+}
+
+</script>
+<style scoped>
+
+</style>
